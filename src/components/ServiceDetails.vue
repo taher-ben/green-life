f6aa5075c925ac88e4a6eda4b7ea5e50ae58bf28
taher-ben <seazer914@gmail.com>
2024-08-12 16:02:22 +0200

    ""

--- src/components/ServiceDetails.vue
+++ src/components/ServiceDetails.vue
@@ -2,29 +2,40 @@
   <div>
     <div class="grid lg:grid-cols-2 grid-cols-1">
       <div class="bg-green-950 p-4 flex flex-col justify-center">
-        <h4 class="text-green-500 underline underline-offset-4">Our Company</h4>
-        <p class="text-white text-xl text-balance">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae consequuntur iure ea
-          expedita exercitationem voluptatibus doloribus quidem quasi earum odio delectus neque quis
-          voluptatem necessitatibus possimus, optio debitis rem fugit.
+        <h4 
+          v-if="checkTranslation(`ourservices.${id}.title`)" 
+          class="text-green-500 underline underline-offset-4"
+        >
+          {{ $t(`ourservices.${id}.title`) }}
+        </h4>
+        <p 
+          v-if="checkTranslation('ourservices.1.head')" 
+          class="text-white text-xl text-balance"
+        >
+          {{ $t('ourservices.1.head') }}
         </p>
       </div>
       <div class="h-full">
         <img class="" src="../assets/background2.jpg" alt="Background image" />
       </div>
     </div>
-    <div>
-      <h2>{{ $t('ourservices.1.title') }}</h2>
-      <div v-for="index in arrayForServices" :key="index">
-        <h3 v-if="checkTranslation(`ourservices.1.title_${index}`)">
-          {{ $t(`ourservices.1.title_${index}`) }}
-        </h3>
-        <ul class="list-disc list-inside">
-          <li v-if="checkTranslation(`ourservices.1.${index}`)">
-          {{ $t(`ourservices.1.${index}`) }}
-          </li>
-        </ul>
-      </div>
+
+    <div v-for="index in arrayForServices" >
+      <h2 
+        :key="index"  
+        v-if="checkTranslation(`ourservices.${id}.${index}.title`)" 
+        class="text-xl text-green-600 py-2 font-bold"
+      >
+        {{ $t(`ourservices.${id}.${index}.title`) }}
+        <p 
+          v-for="indes in towarray"  
+          :key="indes" 
+          :v-if="checkTranslation(`ourservices.${id}.${index}.${indes}`)" 
+          class="text-black text-lg py-1"
+        >
+          {{ $t(`ourservices.${id}.${index}.${indes}`) }}
+        </p>
+      </h2>
     </div>
   </div>
 </template>
@@ -39,14 +50,15 @@ export default {
   },
   data() {
     return {
-      arrayForServices: Array.from({ length: 10 }, (_, i) => i + 1)
+      arrayForServices: Array.from({ length: 5 }, (_, i) => i + 1), // مصفوفة من 1 إلى 5
+      towarray: Array.from({ length: 4 }, (_, i) => i + 1) // مصفوفة من 1 إلى 4
     };
   },
-  methods: {
-    checkTranslation(key) {
-      const translation = this.$t(key);
-      return translation && translation.trim() !== key; 
-    }
+methods: {
+  checkTranslation(key) {
+    const translation = this.$t(key);
+    return translation  && translation.trim() == key;
   }
+}
 }
 </script>
